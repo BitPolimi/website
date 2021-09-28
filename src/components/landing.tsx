93b17aba706fdc47a23ad51feb81e83d0eac594f
algoram <enricomilanese913@gmail.com>
2021-09-28 20:32:54 +0200

    Responsive per mobile, mancano dispositivi grandi

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -7,8 +7,8 @@ import "./landing.scss"
 const Landing = () => {
   return (
     <div className="main-container">
+      <h1>BIT POLIMI</h1>
       <div className="text-container">
-        <h1>BIT POLIMI</h1>
         <p className="disabled">&gt; Progetti</p>
         <p className="second disabled">&gt; Info</p>
         <AnchorLink to="/#contacts" className="third">
