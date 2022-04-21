867fd767e59a2fbad86f6bc307a1302c2a680320
Enrico Milanese <enricomilanese913@gmail.com>
2022-04-21 13:42:48 +0200

    Expandable title bar

--- components/TitleBar.tsx
+++ components/TitleBar.tsx
@@ -1,13 +1,25 @@
-const TitleBar = () => {
+import { forwardRef } from "react";
+import { Ref } from "react";
+import Image from "next/image";
+
+import miniLogo from "../public/minilogo.svg";
+
+interface Props {
+	expanded: boolean;
+}
+
+const TitleBar = forwardRef((props: Props, ref: Ref<HTMLDivElement>) => {
 	return (
-		<div className="titlebar">
-			<div className="minilogo"></div>
+		<div ref={ref} className={`titlebar ${props.expanded ? "expanded" : ""}`}>
+			<div className="minilogo">
+				<Image src={miniLogo} alt="" />
+			</div>
 			<div className="spacer"></div>
-			<a href="">PROJECTS</a>
 			<a href="">INFO</a>
+			<a href="">PROJECTS</a>
 			<a href="">CONTACTS</a>
 		</div>
 	);
-};
+});
 
 export default TitleBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,43 @@
 import { NextPage } from "next";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 import TitleBar from "../components/TitleBar";
 
 import logo from "../public/logo.png";
 
 const Index: NextPage = () => {
+	const titleBarRef = useRef<HTMLDivElement>(null);
+	const [titleBarExpanded, setTitleBarExpanded] = useState(false);
+
+	const handleScroll = (_: Event) => {
+		if (titleBarRef.current != null) {
+			const position = titleBarRef.current.getBoundingClientRect().top;
+
+			if (position == 0) {
+				setTitleBarExpanded(true);
+			} else {
+				setTitleBarExpanded(false);
+			}
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	return (
 		<div className="background">
 			<div className="page">
 				<div className="logo">
 					<Image src={logo} alt="" />
 				</div>
-				<TitleBar />
-				<div className="projects"></div>
+				<TitleBar expanded={titleBarExpanded} ref={titleBarRef} />
 				<div className="info"></div>
+				<div className="projects"></div>
 				<div className="contacts"></div>
 			</div>
 		</div>
